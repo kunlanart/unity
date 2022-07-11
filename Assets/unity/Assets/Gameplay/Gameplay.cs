bcb3f590828cb9d7f405e162dbe4d46a41152caa
kunlanart <kunlanart.pi.63@ubu.ac.th>
2022-07-11 21:36:26 +0700

    Kun

---
+++ Assets/unity/Assets/Gameplay/Gameplay.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gameplay : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.gameObject.name.CompareTo("RedSphere") == 0)
+                {
+                    hit.collider.gameObject.SetActive(false);
+                }
+            }
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.gameObject.name.CompareTo("GreenSphere") == 0)
+                {
+                    hit.collider.gameObject.SetActive(false);
+                }
+            }
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.gameObject.name.CompareTo("BlueSphere") == 0)
+                {
+                    hit.collider.gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+
+}
